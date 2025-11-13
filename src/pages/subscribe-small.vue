40f541327e954ad464ea6d0aa38bb67685105cbf
Minseok Jung <nminseok0917@naver.com>
2025-11-14 02:27:32 +0900

    feat: apply pagination

--- src/pages/subscribe-small.vue
+++ src/pages/subscribe-small.vue
@@ -8,12 +8,10 @@
           <div class="subscribe-header-right">
             <span class="subscribe-management-text">구독관리</span>
             <div class="view-controls">
-              <button type="button" class="view-control-btn" :class="{ active: viewMode === 'grid' }"
-                @click="viewMode = 'grid'">
+              <button type="button" class="view-control-btn" :class="{ active: viewMode === 'grid' }" @click="viewMode = 'grid'">
                 <GridIcon class="icon" />
               </button>
-              <button type="button" class="view-control-btn" :class="{ active: viewMode === 'list' }"
-                @click="viewMode = 'list'">
+              <button type="button" class="view-control-btn" :class="{ active: viewMode === 'list' }" @click="viewMode = 'list'">
                 <ListIcon class="icon" />
               </button>
             </div>
@@ -25,7 +23,7 @@
           <div class="subscribe-video-item" v-for="i in 4" :key="i">
             <div class="subscribe-video-thumbnail">
               <img src="/images/video_thumbnail.png" alt="video-thumbnail" />
-              <div class="subscribe-video-badge">
+              <div v-if="false" class="subscribe-video-badge">
                 <span>1</span>
                 <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M6 2L7.5 4.5L10 5L8 7L8.5 9.5L6 8.5L3.5 9.5L4 7L2 5L4.5 4.5L6 2Z" fill="white" />
@@ -53,24 +51,7 @@
 
         <!-- Pagination -->
         <div class="subscribe-pagination">
-          <button type="button" class="pagination-btn pagination-prev" :class="{ active: currentPage > 1 }">
-            <ChevronLeftIcon class="icon" />
-          </button>
-          <div class="pagination-numbers">
-            <button type="button" class="pagination-number" :class="{ active: currentPage === 1 }"
-              @click="currentPage = 1">1</button>
-            <button type="button" class="pagination-number" :class="{ active: currentPage === 2 }"
-              @click="currentPage = 2">2</button>
-            <button type="button" class="pagination-number" :class="{ active: currentPage === 3 }"
-              @click="currentPage = 3">3</button>
-            <button type="button" class="pagination-number" :class="{ active: currentPage === 4 }"
-              @click="currentPage = 4">4</button>
-            <button type="button" class="pagination-number" :class="{ active: currentPage === 5 }"
-              @click="currentPage = 5">5</button>
-          </div>
-          <button type="button" class="pagination-btn pagination-next" :class="{ active: currentPage < 5 }">
-            <ChevronRightIcon class="icon" />
-          </button>
+          <Pagination :current-page="currentPage" />
         </div>
       </article>
     </div>
@@ -82,8 +63,7 @@ import { ref } from "vue";
 import ActorImage from "@/components/actor/ActorImage.vue";
 import GridIcon from "@/assets/icons/icon20/grid_red.svg";
 import ListIcon from "@/assets/icons/icon20/list.svg";
-import ChevronLeftIcon from "@/assets/icons/icon24/chevron_left.svg";
-import ChevronRightIcon from "@/assets/icons/icon24/chevron_right.svg";
+import Pagination from "@/components/Pagination.vue";
 import MoreVerticalIcon from "@/assets/icons/icon20/more_vertical.svg";
 
 const viewMode = ref("grid");
